89879a9704a0c240469d2ab3e163f95574d33860
ziv talmon <zivtalmon89@gmail.com>
2023-04-07 14:30:51 +0300

    promises for recipes made

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -2,16 +2,26 @@ import {useState,useEffect} from 'react'
 
 const Recipe = () => {
 
+    
     useEffect(()=>{
         const getRecipe = async() => {
             const call = await fetch(`https://api.spoonacular.com/recipes/findByIngredients?apiKey=291adf2def974e05a2e7a225ab807799&ingredients=apples`)
             const res = await call.json()
-            setRecipes([res])
+            console.log(res)
+            setRecipes(res)
         }
         getRecipe()
     },[])
-
+    
     const [recipes,setRecipes] = useState([])
+    const [id,setId] = useState()
+
+    const showId = async(e) => {
+        setId(e.target.id)
+        const call = await fetch(`https://api.spoonacular.com/recipes/${id}/analyzedInstructions?apiKey=291adf2def974e05a2e7a225ab807799`)
+        const res = await call.json()
+        console.log(res);
+    }
 
     return (
         <div>
@@ -19,7 +29,7 @@ const Recipe = () => {
                 recipes.map((item,i) => {
                     return(
                         <div key={i}>
-
+                            <h1 id={item.id} onClick={showId}>{item.title}</h1>
                         </div>
                     )
                 })
@@ -27,7 +37,7 @@ const Recipe = () => {
         </div>
     )
 }
-//instructions address https://api.spoonacular.com/recipes/324694/analyzedInstructions?apiKey=291adf2def974e05a2e7a225ab807799
+// instructions address https://api.spoonacular.com/recipes/324694/analyzedInstructions?apiKey=291adf2def974e05a2e7a225ab807799
 //recipe adresss https://api.spoonacular.com/recipes/findByIngredients?apiKey=291adf2def974e05a2e7a225ab807799&ingredients=apples
 
 export default Recipe

b1a3ad0c911df65de02feed098c893144caa91cf
ziv talmon <zivtalmon89@gmail.com>
2023-10-01 14:34:30 +0300

    working on styling for homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,18 +2,19 @@ const Home = () => {
 
     return (
         <div className="home-div">
-            <h1>
-                Home
-            </h1>
-            <div>
-                <h2>
-                    Welcome!
-                    Our Website offers thounsands of recipes from cool fruit sald to warm and comforting chicken soup!
-                    We invite to join sign up and enjoy the most from our website!
-                    Happy cooking!
-                </h2>
+                <img src='pasta.jpg'></img>
+                <h1>
+                    Home
+                </h1>
+                <div>
+                    <h2>
+                        Welcome!
+                        Our Website offers thounsands of recipes from cool fruit sald to warm and comforting chicken soup!
+                        We invite to join sign up and enjoy the most from our website!
+                        Happy cooking!
+                    </h2>
+                </div>
             </div>
-        </div>
     )
 }
 

7cabead5837a699f4e572102c5a31735ffc3fe7e
ziv talmon <zivtalmon89@gmail.com>
2023-10-23 17:24:02 +0300

    server created

--- server.js
+++ server.js
@@ -1,2 +1,14 @@
-import express from 'express'
+import express, { json, urlencoded } from 'express'
 import cors from 'cors'
+import dotenv from 'dotenv'
+
+const app = express()
+
+app.use(cors())
+
+app.use(json())
+app.use(urlencoded({extended:true}))
+
+app.listen(process.env.PORT||5000, () => {
+    console.log(`run on port ${process.env.PORT}||5000`)
+})
